8270df58f9cf61615fb96a79542c2c47f72c05f3
Lisagit <elisarenataie@gmail.com>
2025-08-20 11:11:51 -0300

    subindo projeto

--- src/appconfig.ts
+++ src/appconfig.ts
@@ -1,8 +1,28 @@
-const SERVER_ROUTES = {
+import express from "express";
+import { SERVER_ROUTES } from "./appConfig.js";
+import UsuarioController from "./controller/UsuarioController.js";
+import EquipamentoController from "./controller/EquipamentoController.js";
+import EmprestimoController from "./controller/EmprestimoController.js";
+const router = express.Router();
 
-    NOVO_USUARIO: '/novo/usuario',
-    ATUALIZAR_USUARIO: '/atualiza/usuario',
-    REMOVER_USUARIO: '/remove/usuario',
-    LISTAR_USUARIO: '/lista/usuario'    
-}
-export { SERVER_ROUTES }
+router.get('/', (req, res) => {
+    res.json({ mensagem: "Rota padrão" })
+});
+
+router.get(SERVER_ROUTES.LISTAR_USUARIO, UsuarioController.todos);
+router.get(SERVER_ROUTES.UNICO_USUARIO, UsuarioController.unico);
+router.post(SERVER_ROUTES.NOVO_USUARIO, UsuarioController.cadastrar);
+router.delete(SERVER_ROUTES.REMOVER_USUARIO, UsuarioController.remover);
+router.put(SERVER_ROUTES.ATUALIZAR_USUARIO, UsuarioController.atualizar);
+
+router.get(SERVER_ROUTES.LISTAR_EQUIPAMENTO, EquipamentoController.todos);
+router.post(SERVER_ROUTES.NOVO_EQUIPAMENTO, EquipamentoController.cadastrar);
+router.delete(SERVER_ROUTES.REMOVER_EQUIPAMENTO, EquipamentoController.remover);
+router.put(SERVER_ROUTES.ATUALIZAR_EQUIPAMENTO, EquipamentoController.atualizar);
+
+router.get(SERVER_ROUTES.LISTAR_EMPRESTIMOS, EmprestimoController.todos);
+router.get(SERVER_ROUTES.LISTAR_EMPRESTIMOS_DETALHADO, EmprestimoController.listarDetalhados);
+router.post(SERVER_ROUTES.NOVO_EMPRESTIMO, EmprestimoController.cadastrar);
+router.put(SERVER_ROUTES.ATUALIZAR_EMPRESTIMO, EmprestimoController.atualizar);
+
+export { router, SERVER_ROUTES };
